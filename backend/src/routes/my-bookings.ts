5387cab3b80509cb3a69420993eb54f3b63de25a
Veeravikaas <63235093+Veeravikaas@users.noreply.github.com>
2024-03-21 11:49:42 +0530

    Update my-bookings.ts

--- backend/src/routes/my-bookings.ts
+++ backend/src/routes/my-bookings.ts
@@ -5,7 +5,7 @@ import { HotelType } from "../shared/types";
 
 const router = express.Router();
 
-// /api/my-bookings
+
 router.get("/", verifyToken, async (req: Request, res: Response) => {
   try {
     const hotels = await Hotel.find({
